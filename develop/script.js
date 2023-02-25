9933572640c1f5dd317d841ea33674a54b2b6157
PhilRug <ruggiero.pj@gmail.com>
2023-02-25 16:17:13 -0500

    JS local storage addition

--- develop/script.js
+++ develop/script.js
@@ -10,9 +10,9 @@ $(function () {
     // useful when saving the description in local storage?
     $('.saveBtn').on('click', function () {
         console.log('working?')
-        var info = $(this).siblings('.description').val();
-        var time = $(this).parent().attribute('id');
-        localStorage.set(info, time);
+        var info = $(this).change('.description').val();
+        var time = $(this).change().attribute('id');
+        localStorage.setItem(info, time);
     })
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
@@ -41,7 +41,16 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
     // TODO: Add code to display the current date in the header of the page.
     var currentDay = dayjs();
     $('#currentDay').text(currentDay.format('MMM D, YYYY'));
